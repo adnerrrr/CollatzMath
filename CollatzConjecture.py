68eb0b6fdb724209796c9f71f45b616a089a74b5
André Lopes <alglopes@id.uff.br>
2026-09-24 16:45:42 -0300

    docker

--- CollatzConjecture.py
+++ CollatzConjecture.py
@@ -3,7 +3,7 @@ def testa():
     max = 0
     num = int(input("\nInsert the number you want to try: "))
     while num != 1:
-        if num % 2 == 0: num = num / 2 # applies the Collatz Conjecture
+        if num % 2 == 0: num = num // 2 # applies the Collatz Conjecture
         else: num = num * 3 + 1
         count += 1 # increases steps taken
         if num > max: max = num # stores max value reached
