dcf3506ab70dc220e63dcc944442429d1222a5db
André Lopes <alglopes@id.uff.br>
2026-09-24 15:34:20 -0300

    comments

--- CollatzConjecture.py
+++ CollatzConjecture.py
@@ -1,19 +1,19 @@
 def testa():
-    cont = 0
+    count = 0
     max = 0
-    num = int(input("\nInsira o numero que deseja testar: "))
+    num = int(input("\nInsert the number you want to try: "))
     while num != 1:
-        if num % 2 == 0: num = num / 2
+        if num % 2 == 0: num = num / 2 # applies the Collatz Conjecture
         else: num = num * 3 + 1
-        cont += 1
-        if num > max: max = num
-    print(f"\nO pico foi {max}, e esse numero precisou de um total de {cont}\n")
+        count += 1 # increases steps taken
+        if num > max: max = num # stores max value reached
+    print(f"\nHighest number reached was {max}, and it needed {count} steps until it reached 1.\n")
 
 def main():
-    s = "s"
-    while s == "s":
+    s = "y"
+    while s == "y":
         testa()
-        s = input("Quer tentar outro numero? [s -> sim] \n-> ")
+        s = input("Want to try another number? [y -> yes] \n-> ")
 
 if __name__ == "__main__":
     main()
